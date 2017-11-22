abcd770d29348e86a0d369bc0d35d1247ae04b9e
Md. Parvez Alam <parveza@ad.digital.com>
2017-11-22 18:27:04 +0530

    Initial Commit

---
+++ iTrust.RnD/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTrust.RnD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+               string test = Guid.NewGuid().ToString("N").ToUpper();
+                Console.WriteLine("test: " + test);
+                string word = Console.ReadLine();
+                bool result = Utility.isAnEnglishWord(word);
+                Console.WriteLine("Result is: " + result);
+                Console.ReadKey();
+            }
+           
+
+        }
+    }
+}
---
+++ iTrust.RnD/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTrust.RnD
+{
+    public static class Utility
+    {
+        public static bool  isAnEnglishWord(string word)
+        {
+            NetSpell.SpellChecker.Dictionary.WordDictionary oDict = new NetSpell.SpellChecker.Dictionary.WordDictionary();
+
+            oDict.DictionaryFile = "en-US.dic";
+            //load and initialize the dictionary 
+            oDict.Initialize();
+            string txtWords = word;
+            NetSpell.SpellChecker.Spelling oSpell = new NetSpell.SpellChecker.Spelling();
+
+            oSpell.Dictionary = oDict;
+            char[] chDelims = { ' ', '\n', '\t', '\r' };
+            foreach (string s in txtWords.Split(chDelims))
+            {
+                if (s.Length > 0 && oSpell.TestWord(s))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
